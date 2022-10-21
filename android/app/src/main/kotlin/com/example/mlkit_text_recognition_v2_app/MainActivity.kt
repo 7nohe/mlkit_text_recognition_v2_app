f8b1f3a6d93fb638548565a553d1c1d253c6610b
Daiki Urata <7nohe.urata@gmail.com>
2022-10-21 14:58:50 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/mlkit_text_recognition_v2_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mlkit_text_recognition_v2_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:mlkit_text_recognition_v2_app/pages/home_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
